caf657e124ac5262c411b788416c63537a316b1e
Dave Cameron <dave.bc@gmail.com>
2014-01-20 10:40:39 -0500

    new rails 4.0.2 project made with ruby 2.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Erma::Application.config.secret_key_base = 'c5f9568f71f8354cea7453d7822c0d9efc502b738819b5e93b93c86e5416a3a38bfc2d40fc72aedf41dcacdb93418f9e3b2f812f78113e5c5f3e5770d79b16c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Erma::Application.config.session_store :cookie_store, key: '_erma_session'
